0c3eacd5787af06cf2fec67c99f8c411a3850c4e
baron_l <baron_l@epitech.eu>
2019-06-13 00:16:50 +0200

    feat(container): upload files inside containers

---
+++ app/docker/views/containers/import/containerImportFilesController.js
@@ -0,0 +1,50 @@
+import angular from "angular";
+
+class ContainerImportFilesController {
+  /* @ngInject */
+  constructor($transition$, ContainerService, Notifications, HttpRequestHelper) {
+    this.$transition$ = $transition$;
+    this.ContainerService = ContainerService;
+    this.Notifications = Notifications;
+    this.HttpRequestHelper = HttpRequestHelper;
+  }
+
+  async $onInit() {
+    this.state = {
+      actionInProgress: false,
+      nodeName: this.$transition$.params().nodeName
+    };
+
+    this.formValues = {
+      UploadFile: null,
+      Path: null
+    };
+
+    const containerId = this.$transition$.params().id;
+    try {
+      this.HttpRequestHelper.setPortainerAgentTargetHeader(this.state.nodeName);
+      this.container = await this.ContainerService.container(containerId);
+    } catch (err) {
+      this.Notifications.error("Failure", err, "Unable retrieve container details");
+    }
+  }
+
+  async uploadFiles() {
+    this.state.actionInProgress = true;
+    const containerId = this.container.Id;
+    const file = this.formValues.UploadFile;
+    const path = this.formValues.Path;
+    try {
+      await this.ContainerService.uploadFiles(containerId, file, path);
+      this.Notifications.success("Files successfully uploaded");
+    } catch (err) {
+      this.Notifications.error("Failure", err, "Unable to upload files");
+    } finally {
+      this.state.actionInProgress = false;
+    }
+	}
+}
+export default ContainerImportFilesController;
+angular
+  .module("portainer.docker")
+  .controller("ContainerImportFilesController", ContainerImportFilesController);
